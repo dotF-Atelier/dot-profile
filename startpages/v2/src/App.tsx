3e601b3f819fa1057f4e945657c5214be146de59
Kingstom <kingstom.chen@gmail.com>
2022-10-23 13:16:05 +0800

    migrate

---
+++ startpages/v2/src/App.tsx
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react"
+import styled from "@emotion/styled/macro"
+
+import { ContentSwitch } from "./components"
+import { Settings } from "./Settings/Settings"
+import { Startpage } from "./Startpage/Startpage"
+
+const Layout = styled.div`
+  ${({ theme: { color } }) => css`
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${color.bg.base};
+    color: ${color.fg.base};
+  `}
+`
+
+const App = () => {
+  return (
+    <Layout>
+      <ContentSwitch leftContent={Startpage} rightContent={Settings} />
+    </Layout>
+  )
+}
+
+export default App
